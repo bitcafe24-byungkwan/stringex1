1715b7571b6ee43f05bae1b7bd0d7b68478dad58
Bking625 <ddiddikk@naver.com>
2019-05-16 12:46:52 +0900

    190516

--- src/main/java/com/cafe24/springex/controller/HelloController.java
+++ src/main/java/com/cafe24/springex/controller/HelloController.java
@@ -1,13 +1,56 @@
 package com.cafe24.springex.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
 	
 	@RequestMapping("/hello")
 	public String hello() {
+		return "/WEB-INF/views/hello.jsp";
+	}
+	
+	@RequestMapping("/hello2")
+	public ModelAndView hello2() {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("email","a@b.c");
+		mav.setViewName("/WEB-INF/views/hello.jsp");
+		
+		return mav;
+	}
+	
+	@RequestMapping("/hello3")
+	public String hello3(Model model) {
+		model.addAttribute("email", "dooly@gmail.com");
+		
+		return "/WEB-INF/views/hello.jsp";
+	}
+	
+	@RequestMapping("/hello4")
+	public String hello4(Model model, 
+			@RequestParam("email") String email,
+			String name 
+			/* annotation value 생략시 변수이름으로 값을 찾는다.*/) {
+		
+		model.addAttribute("email", email);
+		System.out.println(name);
+		return "/WEB-INF/views/hello.jsp";
+	}
+	
+	
+	// 기술침투 (servlet), 비추
+	@RequestMapping("/hello5")
+	public String hello5(Model model, HttpServletRequest request) {
+		String name = request.getParameter("name");
+		String email = request.getParameter("email");
+		model.addAttribute("email", email);
+		System.out.println(name);
 		return "/WEB-INF/views/hello.jsp";
 	}
 }
---
+++ src/main/java/com/cafe24/springex/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.cafe24.springex.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.cafe24.springex.vo.UserVo;
+
+
+
+/**
+ * @RequestMapping
+ * type + mathod
+ */
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	
+	
+	@RequestMapping(value = "/join", method=RequestMethod.GET)
+	// @GetMapping("/join") 완전 동일
+	public String join() {
+		return "/WEB-INF/views/join.jsp";
+	}
+		
+	@RequestMapping(value= {"/join","/j"}, method=RequestMethod.POST)
+	// @PostMapping("/join") 완전 동일
+	public String join(@ModelAttribute UserVo userVo) {
+		if( !valid(userVo)) {
+			return "/WEB-INF/views/join.jsp";
+		}
+		return "redirect:/hello";
+	}
+
+	private boolean valid(UserVo userVo) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
